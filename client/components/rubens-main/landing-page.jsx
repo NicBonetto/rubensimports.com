0f3e221f7e82e6fe7e010332e87f9be595cc191c
Nic Bonetto <nick.bonetto@gmail.com>
2017-11-13 13:17:58 -0800

    Added text to landing page.

--- client/components/rubens-main/landing-page.jsx
+++ client/components/rubens-main/landing-page.jsx
@@ -4,7 +4,18 @@ class LandingPage extends Component {
   render() {
     return (
       <section className="container-fluid" id="landing">
-        
+        <div className="row">
+          <div className="col-md-5 col-md-offset-1">
+            <header className="light-text text-center" id="landing-header">
+              <div>
+                <h3>Mercedes Benz, Porsche, and BMW.</h3>
+              </div>
+              <div>
+                <h2>Unparalled service proven through our honesty, integrity, and quality.</h2>
+              </div>
+            </header>
+          </div>
+        </div>
       </section>
     )
   }
