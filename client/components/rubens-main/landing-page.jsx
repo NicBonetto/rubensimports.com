56e03e817ee17ff71bfc146cedbd7fb5e3dcef5e
Nic Bonetto <nick.bonetto@gmail.com>
2017-11-14 12:42:51 -0800

    Navbar created. #8

--- client/components/rubens-main/landing-page.jsx
+++ client/components/rubens-main/landing-page.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
+import NavBar from './landing-navbar.jsx'
 
 class LandingPage extends Component {
   render() {
     return (
       <section className="container-fluid" id="landing">
+        <NavBar/>
         <div className="row">
           <div className="col-md-5 col-md-offset-1">
             <header className="light-text text-center" id="landing-header">
